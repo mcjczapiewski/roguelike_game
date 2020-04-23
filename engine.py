8fab827cab6ee9162adb60219a4522ea5edf0ccf
Robert Wadelek <robert.wadelek.in@gmail.com>
2020-04-23 19:11:21 +0200

    adding condition for movement

--- engine.py
+++ engine.py
@@ -45,16 +45,29 @@ def read_board_from_file(file_name):
 
 
 def movement(key, player, board):
+    ''' moves the player position, if movement is allowed '''
     key_choice = ['w', 's', 'a', 'd']
     column_next = [0, 0, -1, 1]
     row_next = [-1, 1, 0, 0]
+    walkable = ['.','#','+']
     for option in key_choice:
         if key == option:
+            # next position
+            check_col = player['column_position'] + column_next[key_choice.index(option)]
+            check_row = player['row_position'] + row_next[key_choice.index(option)]
             # validate if after the move, player still on board
-            if player['column_position'] + column_next[key_choice.index(option)] not in range(0, len(board[0])):
+            if check_col not in range(0, len(board[0])):
                 return
-            elif player['row_position'] + row_next[key_choice.index(option)] not in range(0, len(board)):
+            elif check_row not in range(0, len(board)):
+                return
+            elif check_pos_value(check_col, check_row, board) not in walkable:
                 return
             player['column_position'] += column_next[key_choice.index(option)]
             player['row_position'] += row_next[key_choice.index(option)]
             return
+
+
+def check_pos_value(board_col, board_row, board):
+    ''' return what is in the given position on the map '''
+    pos_value = board[board_row][board_col]
+    return pos_value
