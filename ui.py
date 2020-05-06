42924ef916261e575f2f18a795a951c68016008c
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-05-06 12:14:46 +0200

    Add first draft for board-depended printing

--- ui.py
+++ ui.py
@@ -1,4 +1,6 @@
 import interaction
+import engine
+import main
 
 
 def display_board(board):
@@ -8,8 +10,31 @@ def display_board(board):
     Returns:
     Nothing
     '''
+    # for row in board:
+    #     print("".join(row))
+
+    corners = engine.room_corn()
+    monkey = [engine.monkey["row"], engine.monkey["col"]]
+    for item in corners:
+        if (
+            item[0][0] < monkey[0] < item[1][0]
+            and item[1][1] < monkey[1] < item[2][1]
+        ):
+            this_square = item
+            break
+    i = 0
     for row in board:
-        print("".join(row))
+        show_row = ""
+        if this_square[0][0] <= i <= this_square[1][0]:
+            j = 0
+            for point in row:
+                if this_square[1][1] <= j <= this_square[2][1]:
+                    show_row += point
+                else:
+                    show_row += " "
+                j += 1
+        print(show_row)
+        i += 1
 
 
 def display_stats():
