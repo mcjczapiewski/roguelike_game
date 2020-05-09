e33b078c3477204ba4bb52b732e4643c477b5203
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-05-09 20:38:03 +0200

    Correct lexicon and style

--- main.py
+++ main.py
@@ -18,49 +18,58 @@ player = {
     "temp_field": "",
 }
 
-def clear_screen(s):
-    for second in reversed(range(1, s)):
-        print(f'\n\n\n\n\n\tWitaj {name}!')
-        print(f"\n\n\tYour game will begin in \033[91m{second}\033[0m")
-        sleep(1)
-        util.clear_screen()
-        
-def open_text(txt, s=6, check=True):
-    if check == True:
+first_line = "\n\n\n\n\n\t"
+
+
+def open_text(txt, s=6, countdown_screen=False):
+    if countdown_screen is False:
         print(txt)
-        sleep(s)
+        # wciśnięcie klawisza zamiast sleep, żeby można było przejść szybciej dalej
+        print("\n\n\nWciśnij klawisz 'd' aby przejść dalej...")
+        key = util.key_pressed()
+        if key == "d":
+            pass
+        # sleep(s)
         util.clear_screen()
-    if check == False:
+    if countdown_screen is True:
         for second in reversed(range(1, 6)):
-            if check == False:
-                print(f"\n\n\tUważaj, grę zaczniesz za: \033[91m{second}\033[0m")
-                sleep(1)
+            print(f"{first_line}Uważaj, grę zaczniesz za: \033[91m{second}\033[0m")
+            sleep(1)
             util.clear_screen()
+
 
 def main():
     util.clear_screen()
     # player info setup
-    name = input('\n\n\n\n\n\tPodaj swoje imię: ')
+    name = input(f'{first_line}Podaj swoje imię: ')
     interaction.characters["hero"]["name"] = name
     util.clear_screen()
-    text = 'Witaj przybyszu. \n Słyszałem, że zwą Cie '+  name + '\n\n Dzisiaj Twój chrzesniak ma urdziny. Gówniak musi, MUSI dostać świeżaka. \n Rozpieszczony smarkacz.'
+    text = f'{first_line}Witaj przybyszu!\n\tSłyszałem, że zwą Cię {name}.\
+\n\n\tDzisiaj Twój chrześniak ma urodziny. Gówniak musi, MUSI dostać świeżaka.\
+\n\tRozpieszczony smarkacz...'
     open_text(text)
-    text = 'Jak ja nie lubie szczeniaka, ale jak mus to mus, w końcu chrześniak'
+    text = f'{first_line}Jak ja nie lubię szczeniaka, ale jak mus to mus, w końcu chrześniak.'
     open_text(text)
-    text = 'Niestety we wszystkich sklepach z rozjechanym robakiem już nie mają naklejek. Zostaje Ci zebrać w inny sposób te wszystkie naklejki \n\n\n Ruszaj do boju, bo bez tego wstrętnego świeżaka nie uda Ci się wbić na impreże. W końcu 8. urodziny to nie przelewki'
+    text = f'{first_line}Niestety, we wszystkich sklepach z rozjechanym robakiem już nie mają naklejek.\
+\n\tZostaje Ci zebrać w inny sposób te wszystkie naklejki.\
+\n\n\tRuszaj do boju, bo bez tego wstrętnego świeżaka nie uda Ci się wbić na imprezę.\
+\n\tW końcu 8. urodziny to nie przelewki!'
     open_text(text, 20)
-    text = 'przygotuj się na ciężkie boje, bo te wszystkie moherowe berety, Janusze oraz cała gimbaza nie dadzą Ci ich za darmo, \n\n Ale najbardziej musisz uważać na strażników miejskich (cholerne pasożyty, nawet piwa nie mozna się napić) i Madki.\n Z nimi to już nie przelewki \n\n Na szczęście pracownicy biedronki są zawsze po Twojej stronie, w końcu kończyli razem z Tobą studia'
+    text = f'{first_line}Przygotuj się na ciężkie boje, bo te wszystkie moherowe berety,\
+\n\tJanusze oraz cała gimbaza nie dadzą Ci ich za darmo.\
+\n\n\tAle najbardziej musisz uważać na strażników miejskich (cholerne pasożyty, nawet piwa nie można się napić) i Madki.\
+\n\tZ nimi to już nie przelewki.\
+\n\n\tNa szczęście pracownicy biedronki są zawsze po Twojej stronie, w końcu kończyli razem z Tobą studia.'
     open_text(text, 20)
-    text = 'A więc ruszaj do boju, bo PICCOLO i 0,5l wody gazowanej już się dla Ciebie chłodzą'
+    text = f'{first_line}A więc ruszaj do boju, bo PICCOLO i 0,5l wody gazowanej już się dla Ciebie chłodzą!'
     open_text(text, 5)
-    text = ''
-    open_text(text, 5, False)
- 
+    open_text("", 5, True)
+
     # level 1
     board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
     engine.get_spawn_pos(board, player)
     util.clear_screen()
-    
+
     is_running = True
     while is_running:
         engine.put_player_on_board(board, player)
@@ -72,7 +81,7 @@ def main():
             exit_game = ''
             while exit_game not in ['Y', 'N']:
                 util.clear_screen()
-                print('\n\n\n\n\n\tCzy na pewno chcesz wyjsc z gry? Y/N')
+                print(f'{first_line}Czy na pewno chcesz wyjść z gry? Y/N')
                 exit_game = util.key_pressed().upper()
                 if exit_game == 'Y':
                     is_running = False
